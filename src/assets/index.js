3832e46cbc1c06532dbf0432af56c32743272324
MarsIfeanyi <marschalice@gmail.com>
2022-10-28 00:23:25 +0100

    Update Component

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import mars from "./mars.png";
+import githubIcon from "./githubIcon.png";
+import I4GLogo from "./I4GLogo.png";
+import slackIcon from "./slackIcon.png";
+import ZuriIntLogo from "./ZuriIntLogo.png";
+import shareButton from "./shareButton.png";
+
+export { mars, githubIcon, I4GLogo, slackIcon, ZuriIntLogo, shareButton };
