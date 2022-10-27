6634c2a3ad40f883d66498ab69378e046f9301db
MarsIfeanyi <marschalice@gmail.com>
2022-10-28 00:21:02 +0100

    Update Component

--- src/components/PythonBooks.jsx
+++ src/components/PythonBooks.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const PythonBooks = () => {
-  return <div>PythonBooks</div>;
+  return (
+    <div className="sectionContainer bgStyle">
+      <a
+        href="https://books.zuri.team/"
+        id="book__python"
+        className="textStyle"
+      >
+        Python Books
+      </a>
+    </div>
+  );
 };
 
 export default PythonBooks;
