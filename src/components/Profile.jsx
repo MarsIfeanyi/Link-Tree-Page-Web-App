8a5cce5697845e9c70aff4b79deecf12bae41a8d
MarsIfeanyi <marschalice@gmail.com>
2022-10-28 00:20:52 +0100

    Update Component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
+import { mars, shareButton } from "../assets";
+
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div
+      id="profile__img"
+      className="sectionContainer mt-10  bg-white space-y-4 py-4 relative"
+    >
+      <div className="flex flex-row">
+        <img
+          src={mars}
+          alt="profile-Image"
+          className="w-36 ring-2 rounded-full ring-Gray900"
+        />
+
+        <img
+          src={shareButton}
+          alt="shareButton-Icon"
+          className="absolute top-5 right-10 md:right-52 md:w-16 "
+        />
+      </div>
+
+      <h1>Marcellus Ifeanyi</h1>
+    </div>
+  );
 };
 
 export default Profile;
