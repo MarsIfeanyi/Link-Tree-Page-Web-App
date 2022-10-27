032d02d25ba6127928650e868f417937b378cad5
MarsIfeanyi <marschalice@gmail.com>
2022-10-27 04:33:16 +0100

    Initial Commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Profile from "./Profile";
+import BackgroundChecks from "./BackgroundChecks";
+import DesignBooks from "./DesignBooks";
+import Footer from "./Footer";
+import PythonBooks from "./PythonBooks";
+import SlackName from "./SlackName";
+import SocialIcon from "./SocialIcon";
+import TwitterName from "./TwitterName";
+import ZuriBooks from "./ZuriBooks";
+import ZuriTeam from "./ZuriTeam";
+
+export {
+  Profile,
+  BackgroundChecks,
+  DesignBooks,
+  Footer,
+  PythonBooks,
+  SlackName,
+  SocialIcon,
+  TwitterName,
+  ZuriBooks,
+  ZuriTeam,
+};
