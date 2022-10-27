dd06dc61422ada435135d574d5764edbc21a97fb
MarsIfeanyi <marschalice@gmail.com>
2022-10-28 00:23:49 +0100

    Update configuration file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,11 @@ module.exports = {
       colors: {
         Gray900: "#101828",
         bgGray200: "#EAECF0",
+        Gray500: "#667085",
+      },
+
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
       },
     },
   },
