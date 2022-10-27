483abcbdb8a3bad156a4208c0b83f32b698cf7cd
MarsIfeanyi <marschalice@gmail.com>
2022-10-28 00:22:11 +0100

    Update Component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+import { ZuriIntLogo, I4GLogo } from "../assets";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footerStyle">
+      <img src={ZuriIntLogo} alt="Zuri-Internship-Logo" />
+
+      <p className="text-Gray500 font-inter">HNG Internship 9 Frontend Task</p>
+
+      <img src={I4GLogo} alt="I4G-Logo" />
+    </div>
+  );
 };
 
 export default Footer;
