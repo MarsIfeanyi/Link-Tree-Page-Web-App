ff0053427ff1affe9fb96304deb128732a129c26
MarsIfeanyi <marschalice@gmail.com>
2022-10-28 00:21:37 +0100

    Update Component

--- src/components/TwitterName.jsx
+++ src/components/TwitterName.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const TwitterName = () => {
-  return <div>TwitterName</div>;
+  return (
+    <div className="sectionContainer bgStyle ">
+      <a
+        href="https://twitter.com/Mars_Energy"
+        id="twitter"
+        className="textStyle"
+      >
+        Twitter Link{" "}
+      </a>
+    </div>
+  );
 };
 
 export default TwitterName;
