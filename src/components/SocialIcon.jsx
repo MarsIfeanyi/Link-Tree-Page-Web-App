a7fe3aa138e2ae9c1b10604936981a6190a84b56
MarsIfeanyi <marschalice@gmail.com>
2022-10-28 00:21:26 +0100

    Update Component

--- src/components/SocialIcon.jsx
+++ src/components/SocialIcon.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { githubIcon, slackIcon } from "../assets";
 
 const SocialIcon = () => {
-  return <div>SocialIcon</div>;
+  return (
+    <div className="socialStyle">
+      <img src={slackIcon} alt="slackIcon" />
+
+      <img src={githubIcon} alt="githubIcon" />
+    </div>
+  );
 };
 
 export default SocialIcon;
