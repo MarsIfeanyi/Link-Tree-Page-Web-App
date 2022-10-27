e8cd103899ed1ace84d79f07c57431bd59f11441
MarsIfeanyi <marschalice@gmail.com>
2022-10-28 00:20:39 +0100

    Update Component

--- src/components/DesignBooks.jsx
+++ src/components/DesignBooks.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const DesignBooks = () => {
-  return <div>DesignBooks</div>;
+  return (
+    <div className="sectionContainer bgStyle">
+      <a
+        href="https://books.zuri.team/design-rules"
+        id="book__design"
+        className="textStyle"
+      >
+        Design Books
+      </a>
+    </div>
+  );
 };
 
 export default DesignBooks;
