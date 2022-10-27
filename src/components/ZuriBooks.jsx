6b29b57b0261483c71b0b94dd41a2016051325b0
MarsIfeanyi <marschalice@gmail.com>
2022-10-28 00:21:50 +0100

    Update Component

--- src/components/ZuriBooks.jsx
+++ src/components/ZuriBooks.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const ZuriBooks = () => {
-  return <div>ZuriBooks</div>;
+  return (
+    <div className="sectionContainer bgStyle">
+      <a href="https://books.zuri.team/" id="books" className="textStyle">
+        Zuri Books
+      </a>
+    </div>
+  );
 };
 
 export default ZuriBooks;
