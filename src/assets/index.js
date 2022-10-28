0744b24daba427c14200fbd053df6d07f4271b9d
MarsIfeanyi <marschalice@gmail.com>
2022-10-28 01:44:48 +0100

    import and export profile picture

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,5 +4,14 @@ import I4GLogo from "./I4GLogo.png";
 import slackIcon from "./slackIcon.png";
 import ZuriIntLogo from "./ZuriIntLogo.png";
 import shareButton from "./shareButton.png";
+import marsIfeanyi from "./marsIfeanyi.png";
 
-export { mars, githubIcon, I4GLogo, slackIcon, ZuriIntLogo, shareButton };
+export {
+  mars,
+  marsIfeanyi,
+  githubIcon,
+  I4GLogo,
+  slackIcon,
+  ZuriIntLogo,
+  shareButton,
+};
