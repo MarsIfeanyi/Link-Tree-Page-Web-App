77ce1f190ece387f6a61e9dd8872dd051bab09bf
MarsIfeanyi <marschalice@gmail.com>
2022-11-05 14:49:11 +0100

    Style: Add custom styles to config file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,10 @@ module.exports = {
         Gray900: "#101828",
         bgGray200: "#EAECF0",
         Gray500: "#667085",
+        Gray300: "#D0D5DD",
+        Gray600: "#475467",
+        Gray700: "#344054",
+        sendBtn: "#1570EF",
       },
 
       fontFamily: {
